e477f42f2512ace971bc1387274fcc18af1a8102
GopalaKrishnan <99976840+gopalkrishz@users.noreply.github.com>
2022-06-25 11:13:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const multer = require("multer")
+const ejs = require("ejs")
+const path = require("path")
+//initiating the app
+const app = express();
+const PORT=3000;
+//initializing the ejs
+app.set("view engine","ejs")
+//for public folder
+app.use(express.static("./public"))
+//creating a disk storage for my uploaded images with the destination and file name extensions
+const storage= multer.diskStorage({
+    destination:"./public/uploads",
+    filename:function(req,file,cb){
+        cb(null,file.fieldname+"-"+Date.now()+path.extname(file.originalname))
+    }
+});
+const upload=multer({
+    storage:storage,
+    limits:{fileSize:1000000},
+    fileFilter:function(req,file,cb){
+    checkFileType(file,cb);//calling the function
+}
+}).single("myImage")//input name we have given
+
+//function for checking file types
+function checkFileType(file, cb){
+    const filetypes = /jpeg|jpg|png|gif/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
+    if(mimetype && extname){
+      return cb(null,true);
+    } else {
+      cb('Error: Images Only!');
+    }
+  }
+
+app.get("/",function(req,res){
+    res.render("index")
+});
+
+app.post("/upload",(req,res)=>{
+    upload(req,res,function(err){
+        if(err){
+            res.render("index",{msg:err})
+        }else {
+            if(req.file == undefined){
+              res.render('index', {
+                msg: 'Error: No File Selected!'
+              });
+            } else {
+              res.render('index', {
+                msg: 'File Uploaded!',
+                file: `uploads/${req.file.filename}`
+              });
+            }
+    }
+});
+});
+app.listen(PORT,()=>{
+    console.log("port running at server 3000")
+})
